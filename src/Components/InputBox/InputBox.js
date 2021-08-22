6586b2849a0a26ee142787539c1d90a3e81d3c8b
mariamchowdhury.200@gmail.com <mariamchowdhury.200@gmail.com>
2021-08-22 15:05:37 +0600

    done

--- src/Components/InputBox/InputBox.js
+++ src/Components/InputBox/InputBox.js
@@ -6,19 +6,25 @@ const InputBox = () => {
   const { register, handleSubmit } = useForm();
   const [dataA, setDataA] = useState(null);
   const [dataB, setDataB] = useState(null);
+  const [uniqueA, setUniqueA] = useState(null);
+  const [uniqueB, setUniqueB] = useState(null);
+  const [commonItem, setCommonItem] = useState(null);
 
   const onSubmit = (data) => {
     const InputA = data.inputA.split(" ");
     setDataA(InputA);
     const InputB = data.inputB.split(" ");
     setDataB(InputB);
+    // common items in both list
+    let c = InputA.filter((value) => InputB.includes(value));
+    setCommonItem(c);
     //difference between a and b
-    var a = new Set(InputA);
-    var b = new Set(InputB);
-    var differenceA = [...InputA].filter((x) => !b.has(x));
-    console.log(differenceA);
-    var differenceB = [...InputB].filter((y) => !a.has(y));
-    console.log(differenceB);
+    let a = new Set(InputA);
+    let b = new Set(InputB);
+    let differenceA = [...InputA].filter((x) => !b.has(x));
+    setUniqueA(differenceA);
+    let differenceB = [...InputB].filter((y) => !a.has(y));
+    setUniqueB(differenceB);
   };
 
   return (
@@ -27,7 +33,7 @@ const InputBox = () => {
         <div className="row">
           <div className="col-md-6 form-container green-color">
             <form onSubmit={handleSubmit(onSubmit)} className="">
-              <div class="mb-3">
+              <div className="mb-3">
                 <label className="form-label">First Input</label>
                 <input
                   name="inputA"
@@ -50,44 +56,50 @@ const InputBox = () => {
             </form>
           </div>
         </div>
-        <div className="row" id="show">
+        <div className="row pb-5">
           <div className="col-md-3">
-            <div class="card green-color">
-              <div class="card-body ">
-                <h5 class="card-title text-center">Items of list A</h5>
-                <p class="card-text">
+            <div className="card green-color">
+              <div className="card-body ">
+                <h5 className="card-title text-center">Items of list A</h5>
+                <p className="card-text">
                   {dataA && dataA.map((dataA) => <li>{dataA}</li>)}
                 </p>
               </div>
             </div>
           </div>
           <div className="col-md-3">
-            <div class="card green-color">
-              <div class="card-body">
-                <h5 class="card-title   text-center">Items of list B</h5>
-                <p class="card-text">
+            <div className="card green-color">
+              <div className="card-body">
+                <h5 className="card-title   text-center">Items of list B</h5>
+                <p className="card-text">
                   {dataB && dataB.map((dataB) => <li>{dataB}</li>)}
                 </p>
               </div>
             </div>
           </div>
           <div className="col-md-3">
-            <div class="card green-color">
-              <div class="card-body ">
-                <h5 class="card-title text-center">
+            <div className="card green-color">
+              <div className="card-body ">
+                <h5 className="card-title text-center">
                   Items of both list A and B
                 </h5>
-                <p class="card-text"></p>
+                <p className="card-text">
+                  {commonItem &&
+                    commonItem.map((commonItem) => <li>{commonItem}</li>)}
+                </p>
               </div>
             </div>
           </div>
           <div className="col-md-3">
-            <div class="card green-color">
-              <div class="card-body ">
-                <h5 class="card-title text-center">
+            <div className="card green-color">
+              <div className="card-body ">
+                <h5 className="card-title text-center">
                   Unique items in both lists
                 </h5>
-                <p class="card-text"></p>
+                <p className="card-text">
+                  {uniqueA && uniqueA.map((uniqueA) => <li>{uniqueA}</li>)}
+                  {uniqueB && uniqueB.map((uniqueB) => <li>{uniqueB}</li>)}
+                </p>
               </div>
             </div>
           </div>
